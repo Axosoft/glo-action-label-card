8098bd63730268e50c894a74374662f12cc0e2a1
Dan Suceava <dans@axosoft.com>
2019-08-07 10:39:56 -0700

    update action to use list of cards

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# GitHub action to add a label to a Glo Boards card
+# GitHub action to add a label to Glo Boards cards
 
-Use this action to add a label to a card on a [Glo Board](https://www.gitkraken.com/glo).
-The action requires the board ID, card ID, and label name as inputs.
+Use this action to add a label to cards on a [Glo Board](https://www.gitkraken.com/glo).
+The action requires an array of objects with board and card IDs, and the label name as inputs.
 
 ## Requirements
 The action requires an auth token in the form of a PAT that you can create in your GitKraken account.
@@ -9,15 +9,28 @@ See the [Personal Access Tokens](https://support.gitkraken.com/developers/pats/)
 
 This token should be stored in your GitHub repo secrets (in repo Settings -> Secrets).
 
-## Example
-Add a step in your workflow file to perform this action:
+## Inputs
+The `cards` input is an array of objects that contain the board ID and card ID pairs:
+```ts
+interface ICard {
+  boardId: string;
+  cardId: string;
+}
+```
+
+The `labelName` input is the name of a label that already exists in the Glo Board.
+
+## Examples
+Add a step in your workflow file to perform this action and use the output of the `glo-action-parse-links` action:
 ```yaml
     steps:
+    - uses: Axosoft/glo-action-parse-links@v1
+      id: glo-parse
+
     - uses: Axosoft/glo-action-label-card@v1
       with:
         authToken: ${{ secrets.GLO-PAT }}
-        boardID: '12345'
-        cardID: '12345'
+        cards: '${{ steps.glo-parse.outputs.cards }}'
         label: 'Released'
       id: glo-label
 ```
--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,61 @@
 import * as core from '@actions/core';
 import GloSDK from '@axosoft/glo-sdk';
 
+interface ICard {
+  boardId: string;
+  cardId: string;
+}
+
 async function run() {
   const authToken = core.getInput('authToken');
-  const boardID = core.getInput('boardID');
-  const cardID = core.getInput('cardID');
+  const cardsJson = core.getInput('cards');
   const labelName = core.getInput('label');
 
   try {
-    // find the board { id, labels }
-    const board = await GloSDK(authToken).boards.get(boardID, {
-      fields: ['labels']
-    });
-    if (!board) {
-      core.setFailed(`Board ${boardID} not found`);
+    const cards = JSON.parse(cardsJson);
+    if (!cards) {
       return;
     }
 
-    // find the card { id, labels }
-    const card = await GloSDK(authToken).boards.cards.get(boardID, cardID, {
-      fields: ['labels']
-    });
-    if (!card) {
-      core.setFailed(`Card ${cardID} not found`);
-      return;
-    }
+    for (let i = 0; i < cards.length; i++) {
+      const cardData = cards[i] as ICard;
+      const {boardId, cardId} = cardData;
 
-    // find label
-    if (board.labels) {
-      const label = board.labels.find(l => l.name === labelName);
-      if (label) {
-        if (!card.labels) {
-          card.labels = [];
+      // find the board { id, labels }
+      const board = await GloSDK(authToken).boards.get(boardId, {
+        fields: ['labels']
+      });
+      if (!board) {
+        core.setFailed(`Board ${boardId} not found`);
+        continue;
+      }
+
+      // find the card { id, labels }
+      const card = await GloSDK(authToken).boards.cards.get(boardId, cardId, {
+        fields: ['labels']
+      });
+      if (!card) {
+        core.setFailed(`Card ${cardId} not found`);
+        continue;
+      }
+
+      // find label
+      if (board.labels) {
+        const label = board.labels.find(l => l.name === labelName);
+        if (label) {
+          if (!card.labels) {
+            card.labels = [];
+          }
+
+          // add label to the card
+          card.labels.push({
+            id: label.id as string,
+            name: label.name as string
+          });
+
+          // update card
+          await GloSDK(authToken).boards.cards.edit(boardId, cardId, card);
         }
-
-        // add label to the card
-        card.labels.push({
-          id: label.id as string,
-          name: label.name as string
-        });
-
-        // update card
-        await GloSDK(authToken).boards.cards.edit(boardID, cardID, card);
       }
     }
   } catch (error) {
